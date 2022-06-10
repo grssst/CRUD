36e98ecbe853499d0cb2fa53447e05fb997d1a72
grssst <hello.git.0921grsst@gmail.com>
2022-06-10 23:20:07 +0900

    기초 소스 파일 추가

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string.h>
+#include <iostream>
+#include <string.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	char name[100][10]={0}, mail[100][30]={0},phone[100][20]={0};
+	
+    int a,b,i,j,q;
+    int num; 
+	int num_1;
+printf("<회원정보 관리 시스템>\n\n원하는 작업을 선택하세요. ex)1\n");
+
+
+while(1){
+	a=0;
+printf("\n1.회원 추가/제거\n2.회원 조회\n3.종료\n4.갱신\n입력 : ");
+scanf("%d",&a);
+if(a==3) break;
+
+
+}
+	
+	
+	return 0;
+}
